ec062e34d4388d48693a2ea851d1d84b66971add
marcosroman <marcosdromand@gmail.com>
2023-02-01 00:57:49 -0300

    tried to make routes functional but failed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useRoutes } from 'react-router-dom';
 import Main from './views/Main';
 import ProductDetail from './views/ProductDetail';
 import EditProduct from './views/EditProduct';
 
+/*
+const routes = [
+	{path: "/", element: <Main />},
+	{path: "/:id", element: <ProductDetail />},
+	{path: "/:id/edit", element: <EditProduct />}
+]
+*/
+
 const App = () => {
+	/*
+	let element = useRoutes(routes);
+
+	return element;
+	*/
   return (
     <div className="App">
 			<BrowserRouter>
